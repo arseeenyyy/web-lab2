44b503246756dd2668644124cf2efe88d6a2fb72
arseeenyyy <rubtsov113@mail.ru>
2024-10-13 18:37:07 +0300

    hello world n change structure

--- src/main/java/com/github/arseeenyyy/AreaCheckServlet.java
+++ src/main/java/com/github/arseeenyyy/AreaCheckServlet.java
@@ -1,5 +1,9 @@
 package com.github.arseeenyyy;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,7 +11,13 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/check")
 public class AreaCheckServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter(); 
+        try {
+            writer.println("<h2>Hello world servlet</h2>");
+        }finally {
+            writer.close();
+        }
     }
 }
